845211a427ed324cae1bc70604baff140cac8463
Nicolas Urman <urmannicolas@gmail.com>
2021-01-14 11:07:58 +0100

    First commit

---
+++ models/image.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema;
+const ObjectId = Schema.Types.ObjectId;
+
+const imageSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  owner: {
+    type: String,
+    required: true
+  },
+  author: {
+    type: String,
+    required: true
+  }
+  description: {
+    type: String,
+    required: true
+  },
+  imageUrl: {
+    type: String,
+    required: true
+  },
+  selled: {
+    type: Boolean,
+    default: false
+  },
+  category: {
+    type: String,
+    enum: ['Portrait', 'Animal', 'Landscape']
+  }
+
+}, {
+  timestamps: {
+    createdAt: 'created_at',
+    updatedAt: 'updated_at'
+  }
+})
+
+const Image = mongoose.model('Image', imageSchema);
+
+module.exports = Image;
---
+++ models/user.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema;
+const ObjectId = Schema.Types.ObjectId;
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  }, 
+  email: {
+    type: String,
+    required: true
+  },
+  uploadImages: [{
+    type: ObjectId,
+    ref: 'image'
+  }],
+  buyedImages: {
+    type: ObjectId,
+    ref: 'image'
+  }
+}, {
+  timestamps: {
+    createdAt: 'created_at',
+    updatedAt: 'updated_at'
+  }
+})
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
