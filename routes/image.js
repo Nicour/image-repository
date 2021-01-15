fd1913c71e2316b02452ac3c917dc58fa13de381
Nicolas Urman <urmannicolas@gmail.com>
2021-01-15 15:24:45 +0100

    getall and getbyid done

--- routes/image.js
+++ routes/image.js
@@ -18,56 +18,84 @@ cloudinary.config({
 
 router.post('/', isLoggedIn(), async (req, res, next) => {
   const { image } = req.body;
-  cloudinary.uploader.upload(image)
-  .then(async (result) => {
-    const newImage = {
-      name: req.body.imageName,
-      author: req.body.author,
-      owner: req.session.user.username,
-      description: req.body.description,
-      category: req.body.category,
-      imageUrl: result.url,
-      publicId: result.public_id
-    };
-    const createImageInDatabase = await Image.create(newImage);
-    const addImageToUserImagesList = await User.updateOne({ username: req.session.user.username}, { $push: { uploadedImages: createImageInDatabase._id } }, { new: true })
-    res.status(200).json({
-      message: "success",
-      result: result
-    });
-  })
-  .catch((error) => {
-    res.status(500).json({
-      message: "failure",
-      error: error
-    });
+  try {
+    cloudinary.uploader.upload(image)
+    .then(async (result) => {
+      const newImage = {
+        name: req.body.imageName,
+        author: req.body.author,
+        owner: req.session.user.username,
+        description: req.body.description,
+        category: req.body.category,
+        imageUrl: result.url,
+        publicId: result.public_id
+      };
+      const createImageInDatabase = await Image.create(newImage);
+      const addImageToUserImagesList = await User.updateOne({ username: req.session.user.username}, { $push: { uploadedImages: createImageInDatabase._id } }, { new: true })
+      res.status(200).json({
+        result: result
+      });
+    })
+    .catch((error) => {
+      res.status(500).json({
+        error: error
+      });
   });
+  } catch (error) {
+    next(error);
+  }
+  
 });
 
 router.delete('/:id', isLoggedIn(), async (req, res, next) => {
   const { id } = req.params;
-  const image = await Image.findById(id);
-  if(image.owner === req.session.user.username) {
-    const deleteImageFromDatabase = await Image.findByIdAndDelete(id);
-    const deleteImageFromUserImagesList = await User.updateOne({ username: req.session.user.username}, { $pull: { uploadedImages: id } }, { new: true })
-    cloudinary.uploader.destroy(image.publicId)
-    .then(result => {
-      res.status(200).json({
-      message: "success",
-      result: result
+  try {
+    const image = await Image.findById(id);
+    if(image.owner === req.session.user.username) {
+      const deleteImageFromDatabase = await Image.findByIdAndDelete(id);
+      const deleteImageFromUserImagesList = await User.updateOne({ username: req.session.user.username}, { $pull: { uploadedImages: id } }, { new: true });
+      cloudinary.uploader.destroy(image.publicId)
+      .then(result => {
+        res.status(200).json({
+        result: result
+      });
+      })
+      .catch(error => {
+        res.status(500).json({
+          error: error
+        });
+      });
+    } else {
+      res.status(403).json({
+        message: "The image can only be removed by the owner"
+     });
+    }
+  } catch (error) {
+    next(error);
+  } 
+});
+
+router.get('/', isLoggedIn(), async (req, res, next) => {
+  try {
+    const listOfAllImages = await Image.find()
+    res.status(200).json({ 
+      listOfAllImages: listOfAllImages 
     });
-    })
-    .catch(error => {
-      res.status(500).json({
-      message: "failure",
-      error: error
-    });
-    })
-  } else {
-    res.status(403).json({
-      message: "The image can by deleted only by the owner"
-    })
+  } catch (error) {
+    next(error);
   }
 });
+
+router.get('/:id', isLoggedIn(), async (req, res, next) => {
+  const { id } = req.params
+  try {
+    const image = await Image.findById(id);
+    res.status(200).json({ 
+      image: image 
+    });
+  } catch (error) {
+    next(error);
+  }
+})
 
 module.exports = router;
